348be5e1eb890a0519e9caabfe6962ac5dd5cec6
PashkaTurzhanskyi <pavlo.turzhanskiy@gmail.com>
2024-01-10 21:11:57 +0200

    add response with axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 export const App = () => {
+  const [listNews, setListNews] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.polygon.io/v2/reference/news?ticker=AAPL&limit=100&apiKey=KPcpXIk3tqCf5HBomDdRTQR2WTTFUDXy'
+      )
+      .then(({ data }) => {
+        setListNews(data.results);
+        console.log(data.results);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+    <div>
       News list
+      <ul>
+        {listNews.map(item => (
+          <li key={item.id}>
+            <p>Publisher:{item.publisher.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
